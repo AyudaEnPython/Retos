cb6ec0125bcdb83ce2fce15d2d3809da8f94f39d
leugimkm <leugim911@hotmail.com>
2022-05-20 02:02:12 -0500

    update

---
+++ scrimba/digits_of_number.py
@@ -0,0 +1,19 @@
+"""AyudaEnPython: https://www.facebook.com/groups/ayudapython
+
+Even though the group is only for spanish speakers, english speakers
+are welcome.
+-----------------------------------------------------------------------
+    ** PYTHON CODE CHALLENGE - DIGITS OF NUMBER **
+
+Write code to extract digits from input-number to list
+
+input = 56789
+
+output = [5,6,7,8,9]
+
+Remember, there are many ways to solve a problem
+"""
+
+input_ = "56789"
+output = [int(i) for i in input_]
+print(output)
