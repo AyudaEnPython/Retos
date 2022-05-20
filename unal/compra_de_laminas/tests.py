b897f3373b7f59c0589bfdf0468c9d2b763cb937
leugimkm <leugim911@hotmail.com>
2022-05-20 00:04:21 -0500

    u: added tests for alt version

--- unal/compra_de_laminas/tests.py
+++ unal/compra_de_laminas/tests.py
@@ -7,19 +7,31 @@ from main import sol
 
 class Test(unittest.TestCase):
 
-    input_ = (
-        json.loads(
+    inputs = (
+        (
+            json.loads(
             '{"d": 55, "o": 57, "p": 65, "t": 55,' \
             '"s": 73, "f": 89, "e": 72, "k": 65, "y": 65}'
+            ),
+            "g q h t w p y f",
         ),
-        "g q h t w p y f"
+        (
+            json.loads(
+            '{"t": 66, "u": 72, "d": 90, "r": 84,' \
+            '"j": 36, "g": 50, "s": 94, "q": 62, "f": 35}'
+            ),
+            "d p h u i t q",
+        )
+    )
+    outputs = (
+        ("274", "t p y f"),
+        ("290", "d u t q"),
     )
 
     def test_sol(self):
-        self.assertEqual(
-            sol(*self.input_),
-            ("274", "t p y f"),
-        )
+        for inp, out in zip(self.inputs, self.outputs):
+            with self.subTest(inp=inp, out=out):
+                self.assertEqual(sol(*inp), out)
 
 
 if __name__ == "__main__":
